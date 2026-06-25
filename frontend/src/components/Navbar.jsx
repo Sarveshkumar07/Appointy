85cd3e8d447cbb46c817ae9521caa70858eb5ee0
Sarvesh <sarveshmg07@gmail.com>
2026-06-25 16:54:32 +0530

    Fix admin panel link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         {/* ✅ Admin Panel Button - show only on home page  */}
         {location.pathname === '/' && (
           <a
-  href="https://appointy-adminpage.vercel.app/"
+  href= "https://appointy-adminpage.vercel.app/"
   target="_blank"
   rel="noopener noreferrer"
   className='bg-primary text-white text-xs px-4 py-2 rounded-full hover:bg-gray-700 hidden md:block'
